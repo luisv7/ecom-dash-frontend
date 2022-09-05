8fd704fe85c9a2cb5ee8a662645942a86b7556ea
luisv7 <luisvillarreal_7@icloud.com>
2022-09-05 13:33:16 -0700

    added privare routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,32 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
-import Menu from '../components/Menu';
+
+// Store Pages
+import Home from '../store_pages/Home';
+import Product from '../store_pages/Product';
+import Admin from '../pages/Admin';
+
+//Admin Pages
 import Dashboard from '../pages/Dashboard';
 import Products from '../pages/Products';
-import Product from '../pages/Product';
 import CreateProduct from '../pages/CreateProduct';
 import UpdateProduct from '../pages/UpdateProduct';
+import Settings from '../pages/Settings';
 
-
-const Main = () => {
-    const [products , setProducts] = useState(null);
+const Main = ({ user }) => {
+    const [ products, setProducts ] = useState(null);
 
     const API_URL = 'http://localhost:3000/api/products';
+  
+    const PrivateRoute = ({ children, user }) => {
+      if(user){
+        return children
+      }else{
+        return <Navigate to="/"/>
+      }
+    }
 
+      // PRODUCTS
     const getProducts = async () => {
         try {
             const response = await fetch(API_URL);
@@ -21,8 +34,8 @@ const Main = () => {
             setProducts(data);
         } catch (error) {
             console.log(error)
-        }
     }
+}
 
     const createProducts = async (product) => {
         await fetch(API_URL, {
@@ -35,7 +48,7 @@ const Main = () => {
         });
         getProducts();
     }
-
+    
     const updateProduct = async (id, product) => {
         await fetch(`${API_URL}/${id}`, {
             method: 'PUT',
@@ -46,7 +59,7 @@ const Main = () => {
         })
         getProducts();
     }
-
+    
     const deleteProducts = async (id) => {
         try {
             await fetch(`${API_URL}/${id}`, {
@@ -62,18 +75,67 @@ const Main = () => {
 
     useEffect(() => {
         getProducts();
-    },[]);
+    },[])
 
     return (
         <main>
-            <Menu />
+
             <Routes>
-                <Route path='/' element={<Home products={products}/>}/>
+                {/* Main Website */}
+                <Route path='/' element={<Home products={products} />}/>
                 <Route path='/:id' element={<Product products={products}/>}/>
-                <Route path='/dashboard' element={<Dashboard products={products}/>}/>
-                <Route path='/products' element={<Products products={products} deleteProducts={deleteProducts} updateProduct={updateProduct}/>}/>
-                <Route path='/create-product' element={<CreateProduct createProducts={createProducts}/>}/>
-                <Route path='/update-product/:id' element={<UpdateProduct products={products} updateProduct={updateProduct}/>}/>
+                <Route path='/admin' element={<Admin user={user}/>}/>
+
+                {/* Admin Panel */}
+                <Route 
+                    path='/admin/dashboard' 
+                    element={
+                        <PrivateRoute user={user}>
+                            <Dashboard products={products}/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route 
+                    path='/admin/products' 
+                    element={
+                        <PrivateRoute user={user}>
+                            <Products 
+                                products={products} 
+                                deleteProducts={deleteProducts} 
+                                updateProduct={updateProduct}
+                            />
+                        </PrivateRoute>
+                    }
+                    />
+                <Route 
+                    path='/admin/settings' 
+                    element={
+                        <PrivateRoute user={user}>
+                            <Settings/>
+                        </PrivateRoute>
+                    }
+                    />
+                <Route 
+                    path='/admin/create-product' 
+                    element={
+                        <PrivateRoute user={user}>
+                            <CreateProduct 
+                                createProducts={createProducts}
+                            />
+                        </PrivateRoute>
+                    }
+                />
+                <Route 
+                    path='/admin/update-product/:id'
+                    element={
+                        <PrivateRoute user={user}>
+                            <UpdateProduct 
+                                products={products} 
+                                updateProduct={updateProduct}
+                            />
+                        </PrivateRoute>
+                    }
+                />
             </Routes>
         </main>
     )
