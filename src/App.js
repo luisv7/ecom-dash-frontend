00d419de391c507d357a40d46e4d597770aa56c5
luisv7 <luisvillarreal_7@icloud.com>
2022-09-05 13:30:49 -0700

    wip: rendering components based on user login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react';
+import { auth, onAuthStateChanged } from './firebase';
 import Main from './components/Main';
+import Menu from './components/Menu';
 import './App.css';
 
 function App() {
+  const [ user, setUser ] = useState(null);
+
+  useEffect(() => {
+   const unsubscribe = onAuthStateChanged(auth, user => setUser(user));
+   return unsubscribe;
+  }, []);
+
   return (
-      <Main />
-  );
+    <>
+      {
+        user ? 
+        <>
+          <Menu user={user} />
+          <Main user={user} />
+        </>
+        :
+        <Main user={user}/>
+      }
+    </>
+  )
 }
 
 export default App;
