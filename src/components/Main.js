afbc2b9420ed03f571698ec483a17d41ca675b73
luisv7 <luisvillarreal_7@icloud.com>
2022-09-06 17:38:39 -0700

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,7 @@ const Main = ({ user, profiles, createProfile, getProfiles }) => {
     const [ products, setProducts ] = useState(null);
     // const [ publicProducts, setPublicProducts ] = useState(null);
 
-    const API_URL = 'https://ecom-dash-v1.herokuapp.com/admin/api/products';
+    const API_URL = 'https://ecom-dash-v1.herokuapp.com/api/products';
     // const API_PUBLIC = 'http://localhost:3000/api/public';
   
     const PrivateRoute = ({ children, user }) => {
